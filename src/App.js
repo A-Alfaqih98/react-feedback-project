d9a18c92fc999aa706852054f9a6aa35b017d15f
Abdulrahman Alfaqih <86182331+Abdulrahman-alfaqih@users.noreply.github.com>
2022-01-08 12:39:52 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import { useState } from 'react';
+
 import Header from './componenets/Header';
 import FeedbackItems from './componenets/FeedbackItems';
 import FeedbackStats from './componenets/FeedbackStats';
 import AboutPage from './pages/AboutPage';
 
 import { FeedbackProvider } from './context/FeedbackContext';
-import Data from './data/feedbackData';
 import Form from './componenets/Form';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AboutIconLink from './componenets/AboutIconLink';
